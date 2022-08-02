ff9492cd7a7d324df1d7b8ffa78ec1fc28c929a1
kabirpathak <kabirpathak99@gmail.com>
2022-08-03 01:41:03 +0530

    [UPDATE] Added demo to hide/show/toggle elements.

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
+  // 3 properties are this.sourceTarget, this.sourceTargets, and this.hasSourceTarget
   static targets = [ "source" ]
 
   copy(event) {
--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-  static targets = [ "name" ]
+  static targets = [ "name" ] // this.nameTarget, this.nameTargets, and this.hasNameTarget
 
   greet() {
     const element = this.nameTarget
